23f77d570e501fd773a154125a9f0181389165a0
DHeartFlame <39465116+DHeartFlame@users.noreply.github.com>
2019-03-06 13:42:49 -0400

    Add files via upload

--- CodeIgniter-3.1.10/application/config/database.php
+++ CodeIgniter-3.1.10/application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'financiamientodb',
+	'hostname' => SERVER,
+	'username' => USER,
+	'password' => PASSWORD,
+	'database' => NAME,
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
